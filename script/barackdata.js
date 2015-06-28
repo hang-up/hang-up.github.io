8ed16f835714ed6bfa17be3c963f80a69ee901b5
Bob <roberthang@hotmail.fr>
2015-06-28 15:17:29 -0400

    Unreferenced Rainr. Added some hierarchy in folder structure.

--- script/barackdata.js
+++ script/barackdata.js
@@ -38,23 +38,18 @@ var Projects = Object.create(Data, {
         value : new Entity("Trends Of2014", "Projects/Trends2014/thumb.jpg", "Projects/Trends2014/index.html", "2014 was a year of expectations. <br/> Here are its biggest trends." ),
         enumerable : true
     },
-
-    entity_4 : {
-        value: new Entity("Rainr ", "Projects/Rainr/thumb.png", "Projects/Rainr/index.html", "<br>Will you need an umbrella today?"),
-        enumerable : true
-    },
     
-    entity_5 : {
+    entity_4 : {
         value : new Entity("Diamond ", "Projects/Art_Diamond/diamond_thumb.png", "Projects/Art_Diamond/diamond.png", "A diamond is forever. <br/> An essay made with Illustrator."),
         enumerable : true
     },
 
-    entity_6 : {
+    entity_5 : {
         value : new Entity("Made InFrance", "Projects/Art_France/thumb.png", "Projects/Art_France/made_in_france.png", "Made in France on my way to Montreal."),
         enumerable : true
     },
     
-    entity_7 : {
+    entity_6 : {
         value : new Entity("Hamburger ", "Projects/Icon_Hamburger/Hambuger_thumb.png", "Projects/Icon_Hamburger/Hambuger.png", "<br/>Who said burgers weren't sexy?" ),
         enumerable : true
     }
